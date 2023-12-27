0da726c93e2bbfa2d1eeb7e3e022a4e40bb1503a
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2023-12-28 01:10:59 +0300

    first upload

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -10,11 +10,17 @@
 
 <div class="contacts">
 	<div class="row gy-5 g-5">
+		<div class="col-lg-2 col-md-12">
+
+		</div>
 		<div class="col-lg-8 col-md-12">
 			<contact-form-component></contact-form-component>
 		</div>
-		<div class="col-lg-4 col-md-12">
-			<!-- <div class="contacts-info">
+		<div class="col-lg-2 col-md-12">
+
+		</div>
+		<!-- <div class="col-lg-4 col-md-12">
+			<div class="contacts-info">
 				<div class="contact d-flex flex-nowrap align-items-center">
 					<div class="contact-photo" style="background-image: url('{{ asset('img/1_pilot.jpg') }}');"></div>
 					<div class="contact-socials">
@@ -30,8 +36,8 @@
 						<a href="https://t.me/izolda_rally" class="fa-brands fa-whatsapp" target="_blank"></a>
 					</div>
 				</div>
-			</div> -->
-		</div>
+			</div>
+		</div> -->
 	</div>
 </div>
 
