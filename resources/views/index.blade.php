7566324fa8bbb235744f97743441f441520ba240
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2023-12-28 01:07:59 +0300

    first upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,4 +4,12 @@
 
 @section('content')
 
+<div class="title">
+	<h1>Izolda Rally</h1>
+</div>
+
+<center>
+	<iframe width="960" height="508" src="https://www.youtube.com/embed/XSaTRc2xzsM" title="Izolda Rally" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</center>
+
 @endsection
