65a801a7f6f24d6c15113fbe5ace4ba269be098e
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2023-12-28 19:56:18 +0300

    first upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,4 +8,14 @@
 	<h1>Izolda Rally</h1>
 </div>
 
+<div class="index">
+	<div class="row">
+		<div class="col">
+			<div>
+				<img src="{{ asset('img/izolda-crew.jpg') }}" class="w-100 img-thumbnail" />
+			</div>
+		</div>
+	</div>
+</div>
+
 @endsection
