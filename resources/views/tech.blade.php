1c1dc4dd7739003fe21ec9218ed9073e565d2cf8
Roman Kvasnikov <roman.kvasnikov@gmail.com>
2023-12-28 19:58:45 +0300

    first upload

--- resources/views/tech.blade.php
+++ resources/views/tech.blade.php
@@ -8,4 +8,14 @@
 	<h1>Техника</h1>
 </div>
 
+<div class="index">
+	<div class="row">
+		<div class="col">
+			<div>
+				<img src="{{ asset('img/activity/image_2.jpg') }}" class="w-100 img-thumbnail" />
+			</div>
+		</div>
+	</div>
+</div>
+
 @endsection
